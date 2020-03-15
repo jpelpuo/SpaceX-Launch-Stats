9bdf47c33bce0f610c4f8f231ef8fc7413ac650f
Jamal Pelpuo <jpelpuo@ymail.com>
2020-03-15 10:06:48 +0000

    Adds client UI

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }))
 
+app.use(express.static('public'));
+
+app.get('*', (request, response) => {
+    response.sendFile(__dirname, 'public', 'index.html')
+})
 
 module.exports = app;
---
+++ public/precache-manifest.be3a2e8a892367482e6150ab72c1bed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7d6fadf346395d94892f9d343d8f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac74494540b08aaa977",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "41685370baf134ce260a",
+    "url": "/static/js/2.1586868d.chunk.js"
+  },
+  {
+    "revision": "d371590fcabfcefa3eb0ec019be33df2",
+    "url": "/static/js/2.1586868d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac74494540b08aaa977",
+    "url": "/static/js/main.e21cf3ed.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "7dab9996f15513443ab7ec4d1c04cab9",
+    "url": "/static/media/logo.7dab9996.jpg"
+  }
+]);
